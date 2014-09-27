c49bca239a27d2770e71d6d542b2dd44a4522ffc
Riley Woo <mylifeisriley@rileys-mbp.attlocal.net>
2014-09-26 20:50:51 -0700

    first commit

---
+++ MyFirstApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApplication
+//
+//  Created by Riley Woo on 9/9/14.
+//  Copyright (c) 2014 Riley Woo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirstApplication/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyFirstApplication
+//
+//  Created by Riley Woo on 9/9/14.
+//  Copyright (c) 2014 Riley Woo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
